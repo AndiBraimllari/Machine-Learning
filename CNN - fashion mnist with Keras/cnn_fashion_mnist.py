c24dcc2ff079d83dd4f08859c4cd145aab162fde
AndiBraimllari <andibraimllari@gmail.com>
2018-08-07 15:43:33 +0200

    CNN in Keras

---
+++ CNN - fashion mnist with Keras/cnn_fashion_mnist.py
@@ -0,0 +1,36 @@
+import keras
+from keras.layers import MaxPool2D, Conv2D, Dense, Dropout, Flatten
+from keras.optimizers import RMSprop
+from keras.models import Sequential
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train /= 255
+x_test /= 255
+
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPool2D(strides=(2, 2)))
+model.add(Conv2D(64, kernel_size=(5, 5), activation='relu'))
+model.add(MaxPool2D(strides=(2, 2)))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.35))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer=RMSprop(), loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=128, epochs=5)
+score = model.evaluate(x_test, y_test)
+print(score)  # Loss and accuracy
